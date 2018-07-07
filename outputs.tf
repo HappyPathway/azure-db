d44ebe91bd3377b705b2b39f1b4b17bf92671f5e
Dave Arnold <darnold@hashicorp.com>
2018-07-06 17:53:43 -0700

    adding outputs

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "fqdn" {
+  value = "${module.mysql_server.fqdn}"
+}
+
+output "server_name" {
+  value = "${vmodule.mysql_server.server_name}"
+}
+
+output "db_name" {
+  value = "${module.mysql_server.db_name}"
+}
+
+output "vault_token" {
+  value = "${module.mysql_server.vault_token}"
+}
+
+output "vault_cluster" {
+  value = "${module.mysql_server.vault_cluster}"
+}
