e9238cd0d1810e0992c8ce745e6c58a9c3e11de0
Dave Arnold <darnold@hashicorp.com>
2018-07-09 15:31:35 -0700

    bump

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 // Modules
 module "mysql_server" {
   source  = "app.terraform.io/Darnold-Hashicorp/mysql-server/azure"
-  version = "2.0.2"
+  version = "2.0.3"
 
   db_name        = "vaultdemo"
   ip_name        = "simple-app-ip001"
