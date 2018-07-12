fdac90b75a722e64b1ec1341884fd0f585bbdc54
Dave Arnold <darnold@hashicorp.com>
2018-07-12 00:41:02 -0700

    bump

--- main.tf
+++ main.tf
@@ -2,10 +2,9 @@
 // Modules
 module "mysql_server" {
   source  = "app.terraform.io/Darnold-AzureTF/mysql-server/azure"
-  version = "2.3.0"
+  version = "2.4.0"
 
   db_name        = "vaultdemo"
-  ip_name        = "simple-app-ip001"
   location       = "${data.terraform_remote_state.network.location}"
   resource_group = "${data.terraform_remote_state.network.rg_name}"
   server_name    = "${var.service_name}"
